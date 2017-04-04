b7c2b46a5ad96ba3d90125d9f54b58eaeb327bf3
Kimm <kim@mettro.com.au>
2017-04-04 11:43:29 +0000

    initial commit

---
+++ app/api/openWeatherMap.jsx
@@ -0,0 +1,23 @@
+var axios = require('axios');
+
+const OPEN_WEATHER_MAP_URL = '//api.openweathermap.org/data/2.5/weather?units=metric&appid=36a04863a09447666058c833852d1ebe';
+
+
+module.exports = {
+    getTemp: function (location) {
+        var encodedLocation = encodeURIComponent(location);
+        var requestUrl = `${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`;
+        
+        return axios.get(requestUrl)
+            .then(function (res) {
+                if (res.data.cod && res.data.message) {
+                    throw new Error(res.data.message);
+                } 
+                return res.data.main.temp;
+            })
+            .catch(function (res) {
+                throw new Error(res.data.message);
+            });
+        
+    }
+};
---
+++ app/components/WeatherMessage.jsx
@@ -0,0 +1,13 @@
+var React = require("react");
+
+
+var WeatherMessage = ({temp, location}) => {
+    return (
+        <div>
+            <p>It is {temp} in {location}</p>
+        </div>
+    );  
+} 
+
+
+module.exports = WeatherMessage;
